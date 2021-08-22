523e8f60fc0162a9ff73de2dd37f98e26d7b61c5
zubairhmd <zubairhmd84@gmail.com>
2021-08-22 18:59:25 +0600

    final committ

---
+++ README.md
@@ -0,0 +1,1 @@
+# Special_Concept_Clear_Session-22-08-2021
---
+++ main.js
@@ -0,0 +1,6 @@
+// দুইটা string যোগ করব | এটাকে concatenate বলে 
+
+const firstNmae = 'Zubair';
+const lastName = 'Ahmed';
+const fullName = 'Zubair' + ' ' + 'Ahmed';
+console.log(fullName);
